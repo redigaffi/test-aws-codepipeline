5a4a76e482ae6bfe26feab6a953a99c0a9be4ef4
Jordi Walter Hoock Castro <jordixboy@gmail.com>
2018-04-05 12:02:47 +0200

    Make joke bigger

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <h1>AWS Testing CodePipelines v0.0.4</h1>
-<img src="aws.png" />
 
 <?php
 require 'vendor/autoload.php';
 use Joker\MakeAJoke;
 $joker = new MakeAJoke();
-echo $joker->tellMeARandomJoke(false, "Bill", "Gates") . "\n";
+echo "<h2>". $joker->tellMeARandomJoke(false, "Bill", "Gates") . "</h2>\n";
 ?>
+
+<img src="aws.png" />
